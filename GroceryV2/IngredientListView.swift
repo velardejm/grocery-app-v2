72f14bcc77faa3d22cda31de60bd2914a88e57c8
jm <email@email.com>
2023-07-07 00:46:32 +0300

    Add 'Add/Edit' ingredient code.

--- GroceryV2/IngredientListView.swift
+++ GroceryV2/IngredientListView.swift
@@ -5,11 +5,12 @@
 //  Created by JM Velarde on 7/6/23.
 //
 
+import CoreData
 import SwiftUI
 
 struct IngredientListView: View {
     @Environment(\.managedObjectContext) var moc
-
+    
     @FetchRequest(sortDescriptors: []) var ingredientTypes: FetchedResults<IngredientType>
     @FetchRequest(sortDescriptors: []) var ingredients: FetchedResults<Ingredient>
     @FetchRequest(sortDescriptors: []) var ingredientCategories: FetchedResults<IngredientCategory>
@@ -17,7 +18,7 @@ struct IngredientListView: View {
     @State private var name = ""
     @State private var unit = ""
     @State private var quantity: Float?
-    @State private var category = ""
+    @State private var category = "Poultry"
     
     var categoryArray: [String] {
         var categoryArray:[String] = []
@@ -46,15 +47,14 @@ struct IngredientListView: View {
                     }
                 }
                 
-                Button("Add Ingredient") {
-                    let newIngredient = Ingredient(context: moc)
-                    newIngredient.name = name
-                    newIngredient.quantity = Float(quantity ?? 0)
-                    newIngredient.unit = unit
-                    newIngredient.category = category
-                     try? moc.save()
-                    
-                }
+                //                Button("Add Ingredient") {
+                //                    let newIngredient = Ingredient(context: moc)
+                //                    newIngredient.name = name
+                //                    newIngredient.quantity = Float(quantity ?? 0)
+                //                    newIngredient.unit = unit
+                //                    newIngredient.category = category
+                //
+                //                }
                 
             } header: {
                 Text("Add Ingredient")
@@ -69,12 +69,32 @@ struct IngredientListView: View {
             }
             
             Button("Test") {
-                print(categoryArray)
+                if let result = getType() {
+                    print(result.name ?? "")
+                    print(result.category ?? "")
+                }
+                
             }
         }
     }
     
-    
+    func getType() -> IngredientType? {
+        var typeArray: [IngredientType] = []
+        
+        for i in ingredientTypes.indices {
+            typeArray.append(ingredientTypes[i])
+        }
+        
+        let filtered = typeArray.filter({name.contains($0.name ?? "")})
+        
+//        print(filtered.count)
+        
+        if filtered.count > 0 {
+            return filtered[0]
+        }
+        
+        return nil
+    }
     
 }
 
